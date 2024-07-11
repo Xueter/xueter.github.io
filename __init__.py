d4387ce1f72963db6eec1cbf08a01fd387b0c638
Xueter <joseheraldine24@gmail.com>
2024-07-12 00:31:43 +0700

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,88 +1,89 @@
-#!/usr/bin/env python3
-"""Fortran to Python Interface Generator.
-
-Copyright 1999 -- 2011 Pearu Peterson all rights reserved.
-Copyright 2011 -- present NumPy Developers.
-Permission to use, modify, and distribute this software is given under the terms
-of the NumPy License.
-
-NO WARRANTY IS EXPRESSED OR IMPLIED.  USE AT YOUR OWN RISK.
 """
-__all__ = ['run_main', 'get_include']
+``numpy.lib`` is mostly a space for implementing functions that don't
+belong in core or in another NumPy submodule with a clear purpose
+(e.g. ``random``, ``fft``, ``linalg``, ``ma``).
 
-import sys
-import subprocess
-import os
-import warnings
+``numpy.lib``'s private submodules contain basic functions that are used by
+other public modules and are useful to have in the main name-space.
 
-from numpy.exceptions import VisibleDeprecationWarning
-from . import f2py2e
-from . import diagnose
+"""
 
-run_main = f2py2e.run_main
-main = f2py2e.main
+# Public submodules
+# Note: recfunctions and (maybe) format are public too, but not imported
+from . import array_utils
+from . import introspect
+from . import mixins
+from . import npyio
+from . import scimath
+from . import stride_tricks
 
+# Private submodules
+# load module names. See https://github.com/networkx/networkx/issues/5838
+from . import _type_check_impl
+from . import _index_tricks_impl
+from . import _nanfunctions_impl
+from . import _function_base_impl
+from . import _stride_tricks_impl
+from . import _shape_base_impl
+from . import _twodim_base_impl
+from . import _ufunclike_impl
+from . import _histograms_impl
+from . import _utils_impl
+from . import _arraysetops_impl
+from . import _polynomial_impl
+from . import _npyio_impl
+from . import _arrayterator_impl
+from . import _arraypad_impl
+from . import _version
 
-def get_include():
-    """
-    Return the directory that contains the ``fortranobject.c`` and ``.h`` files.
+# numpy.lib namespace members
+from ._arrayterator_impl import Arrayterator
+from ._version import NumpyVersion
+from numpy._core._multiarray_umath import add_docstring, tracemalloc_domain
+from numpy._core.function_base import add_newdoc
 
-    .. note::
+__all__ = [
+    "Arrayterator", "add_docstring", "add_newdoc", "array_utils",
+    "introspect", "mixins", "NumpyVersion", "npyio", "scimath",
+    "stride_tricks", "tracemalloc_domain"
+]
 
-        This function is not needed when building an extension with
-        `numpy.distutils` directly from ``.f`` and/or ``.pyf`` files
-        in one go.
-
-    Python extension modules built with f2py-generated code need to use
-    ``fortranobject.c`` as a source file, and include the ``fortranobject.h``
-    header. This function can be used to obtain the directory containing
-    both of these files.
-
-    Returns
-    -------
-    include_path : str
-        Absolute path to the directory containing ``fortranobject.c`` and
-        ``fortranobject.h``.
-
-    Notes
-    -----
-    .. versionadded:: 1.21.1
-
-    Unless the build system you are using has specific support for f2py,
-    building a Python extension using a ``.pyf`` signature file is a two-step
-    process. For a module ``mymod``:
-
-    * Step 1: run ``python -m numpy.f2py mymod.pyf --quiet``. This
-      generates ``mymodmodule.c`` and (if needed)
-      ``mymod-f2pywrappers.f`` files next to ``mymod.pyf``.
-    * Step 2: build your Python extension module. This requires the
-      following source files:
-
-      * ``mymodmodule.c``
-      * ``mymod-f2pywrappers.f`` (if it was generated in Step 1)
-      * ``fortranobject.c``
-
-    See Also
-    --------
-    numpy.get_include : function that returns the numpy include directory
-
-    """
-    return os.path.join(os.path.dirname(__file__), 'src')
-
+from numpy._pytesttester import PytestTester
+test = PytestTester(__name__)
+del PytestTester
 
 def __getattr__(attr):
+    # Warn for reprecated attributes
+    import math
+    import warnings
 
-    # Avoid importing things that aren't needed for building
-    # which might import the main numpy module
-    if attr == "test":
-        from numpy._pytesttester import PytestTester
-        test = PytestTester(__name__)
-        return test
-
+    if attr == "math":
+        warnings.warn(
+            "`np.lib.math` is a deprecated alias for the standard library "
+            "`math` module (Deprecated Numpy 1.25). Replace usages of "
+            "`numpy.lib.math` with `math`", DeprecationWarning, stacklevel=2)
+        return math
+    elif attr == "emath":
+        raise AttributeError(
+            "numpy.lib.emath was an alias for emath module that was removed "
+            "in NumPy 2.0. Replace usages of numpy.lib.emath with "
+            "numpy.emath."
+        )
+    elif attr in (
+        "histograms", "type_check", "nanfunctions", "function_base",
+        "arraypad", "arraysetops", "ufunclike", "utils", "twodim_base",
+        "shape_base", "polynomial", "index_tricks",
+    ):
+        raise AttributeError(
+            f"numpy.lib.{attr} is now private. If you are using a public "
+            "function, it should be available in the main numpy namespace, "
+            "otherwise check the NumPy 2.0 migration guide."
+        )
+    elif attr == "arrayterator":
+        raise AttributeError(
+            "numpy.lib.arrayterator submodule is now private. To access "
+            "Arrayterator class use numpy.lib.Arrayterator."
+        )
     else:
         raise AttributeError("module {!r} has no attribute "
-                              "{!r}".format(__name__, attr))
-
-
-def __dir__():
-    return list(globals().keys() | {"test"})
+                             "{!r}".format(__name__, attr))
--- __init__.pyi
+++ __init__.pyi
@@ -1,42 +1,41 @@
-import os
-import subprocess
-from collections.abc import Iterable
-from typing import Literal as L, Any, overload, TypedDict
+import math as math
 
 from numpy._pytesttester import PytestTester
 
-class _F2PyDictBase(TypedDict):
-    csrc: list[str]
-    h: list[str]
+from numpy import (
+    ndenumerate as ndenumerate,
+    ndindex as ndindex,
+)
 
-class _F2PyDict(_F2PyDictBase, total=False):
-    fsrc: list[str]
-    ltx: list[str]
+from numpy.version import version
+
+from numpy.lib import (
+    format as format,
+    mixins as mixins,
+    scimath as scimath,
+    stride_tricks as stride_tricks,
+    npyio as npyio,
+    array_utils as array_utils,
+)
+
+from numpy.lib._version import (
+    NumpyVersion as NumpyVersion,
+)
+
+from numpy.lib._arrayterator_impl import (
+    Arrayterator as Arrayterator,
+)
+
+from numpy._core.multiarray import (
+    add_docstring as add_docstring,
+    tracemalloc_domain as tracemalloc_domain,
+)
+
+from numpy._core.function_base import (
+    add_newdoc as add_newdoc,
+)
 
 __all__: list[str]
 test: PytestTester
 
-def run_main(comline_list: Iterable[str]) -> dict[str, _F2PyDict]: ...
-
-@overload
-def compile(  # type: ignore[misc]
-    source: str | bytes,
-    modulename: str = ...,
-    extra_args: str | list[str] = ...,
-    verbose: bool = ...,
-    source_fn: None | str | bytes | os.PathLike[Any] = ...,
-    extension: L[".f", ".f90"] = ...,
-    full_output: L[False] = ...,
-) -> int: ...
-@overload
-def compile(
-    source: str | bytes,
-    modulename: str = ...,
-    extra_args: str | list[str] = ...,
-    verbose: bool = ...,
-    source_fn: None | str | bytes | os.PathLike[Any] = ...,
-    extension: L[".f", ".f90"] = ...,
-    full_output: L[True] = ...,
-) -> subprocess.CompletedProcess[bytes]: ...
-
-def get_include() -> str: ...
+__version__ = version
