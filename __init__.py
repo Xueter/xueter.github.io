7ce35797916c8a07cd91351f33def801425a9d65
Xueter <joseheraldine24@gmail.com>
2024-07-11 23:57:53 +0700

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,181 +1,60 @@
-'''
-OpenCV Python binary extension loader
-'''
-import os
-import importlib
-import sys
+from __future__ import annotations
 
-__all__ = []
+import typing as t
 
-try:
-    import numpy
-    import numpy.core.multiarray
-except ImportError:
-    print('OpenCV bindings requires "numpy" package.')
-    print('Install it via command:')
-    print('    pip install numpy')
-    raise
-
-# TODO
-# is_x64 = sys.maxsize > 2**32
-
-
-def __load_extra_py_code_for_module(base, name, enable_debug_print=False):
-    module_name = "{}.{}".format(__name__, name)
-    export_module_name = "{}.{}".format(base, name)
-    native_module = sys.modules.pop(module_name, None)
-    try:
-        py_module = importlib.import_module(module_name)
-    except ImportError as err:
-        if enable_debug_print:
-            print("Can't load Python code for module:", module_name,
-                  ". Reason:", err)
-        # Extension doesn't contain extra py code
-        return False
-
-    if base in sys.modules and not hasattr(sys.modules[base], name):
-        setattr(sys.modules[base], name, py_module)
-    sys.modules[export_module_name] = py_module
-    # If it is C extension module it is already loaded by cv2 package
-    if native_module:
-        setattr(py_module, "_native", native_module)
-        for k, v in filter(lambda kv: not hasattr(py_module, kv[0]),
-                           native_module.__dict__.items()):
-            if enable_debug_print: print('    symbol({}): {} = {}'.format(name, k, v))
-            setattr(py_module, k, v)
-    return True
+from . import json as json
+from .app import Flask as Flask
+from .blueprints import Blueprint as Blueprint
+from .config import Config as Config
+from .ctx import after_this_request as after_this_request
+from .ctx import copy_current_request_context as copy_current_request_context
+from .ctx import has_app_context as has_app_context
+from .ctx import has_request_context as has_request_context
+from .globals import current_app as current_app
+from .globals import g as g
+from .globals import request as request
+from .globals import session as session
+from .helpers import abort as abort
+from .helpers import flash as flash
+from .helpers import get_flashed_messages as get_flashed_messages
+from .helpers import get_template_attribute as get_template_attribute
+from .helpers import make_response as make_response
+from .helpers import redirect as redirect
+from .helpers import send_file as send_file
+from .helpers import send_from_directory as send_from_directory
+from .helpers import stream_with_context as stream_with_context
+from .helpers import url_for as url_for
+from .json import jsonify as jsonify
+from .signals import appcontext_popped as appcontext_popped
+from .signals import appcontext_pushed as appcontext_pushed
+from .signals import appcontext_tearing_down as appcontext_tearing_down
+from .signals import before_render_template as before_render_template
+from .signals import got_request_exception as got_request_exception
+from .signals import message_flashed as message_flashed
+from .signals import request_finished as request_finished
+from .signals import request_started as request_started
+from .signals import request_tearing_down as request_tearing_down
+from .signals import template_rendered as template_rendered
+from .templating import render_template as render_template
+from .templating import render_template_string as render_template_string
+from .templating import stream_template as stream_template
+from .templating import stream_template_string as stream_template_string
+from .wrappers import Request as Request
+from .wrappers import Response as Response
 
 
-def __collect_extra_submodules(enable_debug_print=False):
-    def modules_filter(module):
-        return all((
-             # module is not internal
-             not module.startswith("_"),
-             not module.startswith("python-"),
-             # it is not a file
-             os.path.isdir(os.path.join(_extra_submodules_init_path, module))
-        ))
-    if sys.version_info[0] < 3:
-        if enable_debug_print:
-            print("Extra submodules is loaded only for Python 3")
-        return []
+def __getattr__(name: str) -> t.Any:
+    if name == "__version__":
+        import importlib.metadata
+        import warnings
 
-    __INIT_FILE_PATH = os.path.abspath(__file__)
-    _extra_submodules_init_path = os.path.dirname(__INIT_FILE_PATH)
-    return filter(modules_filter, os.listdir(_extra_submodules_init_path))
+        warnings.warn(
+            "The '__version__' attribute is deprecated and will be removed in"
+            " Flask 3.1. Use feature detection or"
+            " 'importlib.metadata.version(\"flask\")' instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return importlib.metadata.version("flask")
 
-
-def bootstrap():
-    import sys
-
-    import copy
-    save_sys_path = copy.copy(sys.path)
-
-    if hasattr(sys, 'OpenCV_LOADER'):
-        print(sys.path)
-        raise ImportError('ERROR: recursion is detected during loading of "cv2" binary extensions. Check OpenCV installation.')
-    sys.OpenCV_LOADER = True
-
-    DEBUG = False
-    if hasattr(sys, 'OpenCV_LOADER_DEBUG'):
-        DEBUG = True
-
-    import platform
-    if DEBUG: print('OpenCV loader: os.name="{}"  platform.system()="{}"'.format(os.name, str(platform.system())))
-
-    LOADER_DIR = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
-
-    PYTHON_EXTENSIONS_PATHS = []
-    BINARIES_PATHS = []
-
-    g_vars = globals()
-    l_vars = locals().copy()
-
-    if sys.version_info[:2] < (3, 0):
-        from . load_config_py2 import exec_file_wrapper
-    else:
-        from . load_config_py3 import exec_file_wrapper
-
-    def load_first_config(fnames, required=True):
-        for fname in fnames:
-            fpath = os.path.join(LOADER_DIR, fname)
-            if not os.path.exists(fpath):
-                if DEBUG: print('OpenCV loader: config not found, skip: {}'.format(fpath))
-                continue
-            if DEBUG: print('OpenCV loader: loading config: {}'.format(fpath))
-            exec_file_wrapper(fpath, g_vars, l_vars)
-            return True
-        if required:
-            raise ImportError('OpenCV loader: missing configuration file: {}. Check OpenCV installation.'.format(fnames))
-
-    load_first_config(['config.py'], True)
-    load_first_config([
-        'config-{}.{}.py'.format(sys.version_info[0], sys.version_info[1]),
-        'config-{}.py'.format(sys.version_info[0])
-    ], True)
-
-    if DEBUG: print('OpenCV loader: PYTHON_EXTENSIONS_PATHS={}'.format(str(l_vars['PYTHON_EXTENSIONS_PATHS'])))
-    if DEBUG: print('OpenCV loader: BINARIES_PATHS={}'.format(str(l_vars['BINARIES_PATHS'])))
-
-    applySysPathWorkaround = False
-    if hasattr(sys, 'OpenCV_REPLACE_SYS_PATH_0'):
-        applySysPathWorkaround = True
-    else:
-        try:
-            BASE_DIR = os.path.dirname(LOADER_DIR)
-            if sys.path[0] == BASE_DIR or os.path.realpath(sys.path[0]) == BASE_DIR:
-                applySysPathWorkaround = True
-        except:
-            if DEBUG: print('OpenCV loader: exception during checking workaround for sys.path[0]')
-            pass  # applySysPathWorkaround is False
-
-    for p in reversed(l_vars['PYTHON_EXTENSIONS_PATHS']):
-        sys.path.insert(1 if not applySysPathWorkaround else 0, p)
-
-    if os.name == 'nt':
-        if sys.version_info[:2] >= (3, 8):  # https://github.com/python/cpython/pull/12302
-            for p in l_vars['BINARIES_PATHS']:
-                try:
-                    os.add_dll_directory(p)
-                except Exception as e:
-                    if DEBUG: print('Failed os.add_dll_directory(): '+ str(e))
-                    pass
-        os.environ['PATH'] = ';'.join(l_vars['BINARIES_PATHS']) + ';' + os.environ.get('PATH', '')
-        if DEBUG: print('OpenCV loader: PATH={}'.format(str(os.environ['PATH'])))
-    else:
-        # amending of LD_LIBRARY_PATH works for sub-processes only
-        os.environ['LD_LIBRARY_PATH'] = ':'.join(l_vars['BINARIES_PATHS']) + ':' + os.environ.get('LD_LIBRARY_PATH', '')
-
-    if DEBUG: print("Relink everything from native cv2 module to cv2 package")
-
-    py_module = sys.modules.pop("cv2")
-
-    native_module = importlib.import_module("cv2")
-
-    sys.modules["cv2"] = py_module
-    setattr(py_module, "_native", native_module)
-
-    for item_name, item in filter(lambda kv: kv[0] not in ("__file__", "__loader__", "__spec__",
-                                                           "__name__", "__package__"),
-                                  native_module.__dict__.items()):
-        if item_name not in g_vars:
-            g_vars[item_name] = item
-
-    sys.path = save_sys_path  # multiprocessing should start from bootstrap code (https://github.com/opencv/opencv/issues/18502)
-
-    try:
-        del sys.OpenCV_LOADER
-    except Exception as e:
-        if DEBUG:
-            print("Exception during delete OpenCV_LOADER:", e)
-
-    if DEBUG: print('OpenCV loader: binary extension... OK')
-
-    for submodule in __collect_extra_submodules(DEBUG):
-        if __load_extra_py_code_for_module("cv2", submodule, DEBUG):
-            if DEBUG: print("Extra Python code for", submodule, "is loaded")
-
-    if DEBUG: print('OpenCV loader: DONE')
-
-
-bootstrap()
+    raise AttributeError(name)
